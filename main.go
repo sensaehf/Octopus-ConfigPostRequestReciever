93de011b3217b41473f22cb401a13e604db78b54
Tommi-Sensa <131780500+Tommi-Sensa@users.noreply.github.com>
2024-07-05 09:34:40 +0000

    Create basic payload reciever

--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
+type Payload struct {
+	ScanFileName    string `json:ScanFileName""`
+	ScanDescription string `json:ScanDescription",omitempty"`
+	Address         string `json:Address""`
+	Username        string `json:Username""`
+	Password        string `json:Password""`
+	DomainScan      bool   `json:DomainScan""`
+	Customer        string `json:Customer""`
+}
+
 func recieveInfo(w http.ResponseWriter, req *http.Request) {
+	var p Payload
+	decoder := json.NewDecoder(req.Body)
+	decoder.Decode(&p)
+	log.Println(p)
 
 }
 
 func main() {
-
 	http.HandleFunc("/", recieveInfo)
-
 	http.ListenAndServe(":8090", nil)
 }
