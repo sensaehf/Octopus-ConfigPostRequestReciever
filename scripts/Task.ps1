0765d94b852567be023d60ab95d188c4e1d67899
tomottos <tomas.ottosson@sensa.is>
2024-07-24 17:06:00 +0000

    Task script Saved

--- scripts/Task.ps1
+++ scripts/Task.ps1
@@ -1,21 +1,15 @@
-#Declarejob that enables and disables the gmsa after 10 minutes
-# Define the job trigger to run every minute
-$T= New-JobTrigger -Once -At (Get-Date).AddMinutes(1) -RepetitionInterval (New-TimeSpan -Minutes 1) -RepetitionDuration ([TimeSpan]::MaxValue)
-$O = New-ScheduledJobOption -RunElevated -MultipleInstancePolicy "IgnoreNew" -RequireNetwork -StartIfOnBattery -DoNotAllowDemandStart
-      
-Register-ScheduledJob -Name "ReadNewConfig" -Trigger $T -ScheduledJobOption $O -ScriptBlock {
-    try{
-        $arguments = get-content "C:\Admin\ConfigData\data.txt" -Raw -ErrorAction Stop
-        $path = "C:\inetpub\oc_configurator\configs\OctopusConfigurator.exe"
-        Start-Process -FilePath $path -ArgumentList $arguments -Verb RunAs
-        Remove-Item "C:\Admin\ConfigData\data.txt"
-        } catch{}
-    }     
-      
+try{
+    $arguments = get-content "C:\Admin\ConfigData\data.txt" -Raw -ErrorAction Stop
+    Remove-Item "C:\Admin\ConfigData\data.txt" 
+    $x = $arguments -split "-"
+    $x = $x[1] -split" "
+    $x = $x[1]+".cfg"
+    if(-not (Test-Path ("C:\inetpub\oc_configurator\configs\"+$x) -PathType Leaf) ){
+           cd C:\inetpub\oc_configurator\configs
+           Start-Process .\OctopusConfigurator.exe -ArgumentList $arguments
+    }
+    } catch{
+        Write-Host "No Data File avaiable"
+    } 
 
-#Set so GMSA runs the exe
-$P = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest;
-$psJobsPathInScheduler = "\Microsoft\Windows\PowerShell\ScheduledJobs";
-Set-ScheduledTask -TaskPath $psJobsPathInScheduler -TaskName "ReadNewConfig" -Principal $P
-Write-Host "Task created under Microsoft\Windows\PowerShell\ScheduledJobs" -ForegroundColor Green
-      
+# Registerd as a task on the server running as a service account
